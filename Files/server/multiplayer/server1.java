9a4f1d9d30e133afc81e7a243b47f631637baafb
MatrixD14 <166034702+MatrixD14@users.noreply.github.com>
2025-10-04 19:18:24 -0400

    comito

--- Files/server/multiplayer/server1.java
+++ Files/server/multiplayer/server1.java
@@ -116,14 +116,14 @@ public class server1 extends Component {
           clientName[slot] = nome;
           sendClient(client, "id:" + clientId[slot]);
           for (int i = 0; i < maxPlayer; i++) {
-            if (clients[slot] != null && i != slot) {
+            if (clients[slot] != null && i != slot && clientId[i] != 0 && clientName[i] != null) {
               String exists = "spaw:" + clientId[i] + ":" + clientName[i] + ":0:1:0";
-              broadcast(exists,client);
+              sendClient(client,exists);
             }
           }
           String spaw = "spaw:" + clientId[slot] + ":" + nome + ":0:1:0";
           broadcast(spaw,client);
-        } 
+        }else if(line.startsWith("pos:"))broadcast(line, client);
         broadcast(line, client);
       }
     } catch (Exception e) {
