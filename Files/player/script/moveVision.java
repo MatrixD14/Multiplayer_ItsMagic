7863dc5a955eefe0c6bf4d229c9dfc2fc7e5e656
MatrixD14 <166034702+MatrixD14@users.noreply.github.com>
2026-01-15 10:54:00 -0400

    comito

--- Files/player/script/moveVision.java
+++ Files/player/script/moveVision.java
@@ -1,28 +1,27 @@
 public class moveVision extends Component {
-  private Vector2 vision, visionMouse, joy;
-  private Characterbody charac = null;
-  private float speedJ = 3, speedS = 1f, camv, camh;
-  public float x, y;
-
+  private Vector2 joy, slid;
+  public Characterbody ch;
+  private float speedJ = 20;
+  private float x, y, camx, camy;
+  public SpatialObject cam, myobj,chao;
+  private float cammin = -80, cammax = 80;
+  public UITextView t;
   void start() {
-    charac = myObject.getPhysics().getPhysicsEntity();
-    joy = Input.getAxisValue("joy");
-    vision = Input.getAxisValue("vision");
+    myobj = myObject;
+    ch = myobj.findComponent("Characterbody");
+    cam = myObject.findChildObject("vision");
+    t = WorldController.findObject("TextView").findComponent("textview");
+    chao =WorldController.findObject("chao");
   }
 
   void repeat() {
-    visionMouse = new Vector2(Input.mouse.getSlideX(), Input.mouse.getSlideY());
-    x = Input.mouse.getSlideX();
-    y = Input.mouse.getSlideY();
-    if (key("w") || key("s") || key("a") || key("d")) {
-      movekey();
-    } else move(joy.x * speedJ, joy.y * speedJ);
-
-    /*    if (Input.mouse.isConnected() && !Input.getTouch(0).isPressed()) {
-      vision(x < -20 && x > 20 ? 0 : x * speedS, y < -5 && y > 5 ? 0 : y * speedS);
-    } else */
-    vision(vision.x * speedS, vision.y * speedS);
-  }
+    if (joy == null) joy = Input.registerAxis("joy").getValue();
+    if (slid == null) slid = Input.registerAxis("slid").getValue();
+    if (key("w") || key("s") || key("a") || key("d")) movekey();
+    else move(joy.x * speedJ, joy.y * speedJ);
+    t.setText(""+(int)(Math.abs(chao.globalPosition.y-myobj.globalPosition.y)));
+    slidCam(slid.x, slid.y);
+  } 
 
   private void movekey() {
     float x = 0, y = 0;
@@ -30,8 +29,8 @@ public class moveVision extends Component {
     if (key("s")) y = -1;
     if (key("a")) x = -1;
     if (key("d")) x = +1;
-    move(x * speedJ, y * speedJ);
-  } 
+    move(x * speedJ, -y * speedJ);
+  }
 
   private boolean key(String key) {
     if (Input.keyboard.isKeyPressed(key)) return true;
@@ -39,13 +38,33 @@ public class moveVision extends Component {
   }
 
   private void move(float x, float y) {
-    charac.setSpeed(-x, -y);
+    ch.setSpeed(-x, y);
   }
 
-  private void vision(float x, float y) {
-    camh = Math.clamp(-80, camh += y, 45);
-    myObject.findChildObject("vision").getRotation().selfLookTo(new Vector3(0, Math.sin(-camh), Math.cos(-camh)));
-    camv += x;
-    myObject.getRotation().selfLookTo(new Vector3(Math.sin(-camv), 0, Math.cos(-camv)));
+  private void slidCam(float x, float y) {
+    camx += x;
+    myobj.getRotation().selfLookTo(new Vector3(Math.sin(-camx), 0, Math.cos(-camx)));
+    camy = Math.clamp(cammin, (camy += y), cammax);
+    cam.getRotation().selfLookTo(new Vector3(0, Math.sin(-camy), Math.cos(-camy)));
   }
-}
+
+  /*
+  float shakeAmount = .15f, times = 0;
+  public boolean onoff = true;
+
+  private void shake(float value) {
+    if (!onoff) return;
+    Vector3 mypos = obj.position.copy();
+    if (onoff) {
+      times += 0.01f;
+      float offsetX = (float) Random.range(-shakeAmount, shakeAmount);
+      float offsetZ = (float) Random.range(-shakeAmount, shakeAmount);
+      obj.setPosition(mypos.x + offsetX, mypos.y, mypos.z + offsetZ);
+      if (times > value) {
+        obj.setPosition(mypos);
+        onoff = false;
+        times = 0;
+      }
+    }
+  }*/
+} // 0.329f
